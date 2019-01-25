a2c0cc9aafd84b082670d69b9639cab5e3f125d4
Siim Liimand <siim.liimand@gmail.com>
2019-01-25 17:22:00 +0200

    Improve AbstractListDto

--- src/Dto/AbstractListDto.php
+++ src/Dto/AbstractListDto.php
@@ -8,24 +8,124 @@
 
 namespace Sf4\Api\Dto;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Sf4\Api\Dto\Traits\PaginationTrait;
+
 abstract class AbstractListDto extends AbstractDto
 {
-    /** @var array $items */
+
+    use PaginationTrait;
+
+    /** @var ArrayCollection $items */
     protected $items;
+
+    /** @var int $total */
+    protected $total = 0;
+
+    /** @var int $count */
+    protected $count = 0;
+
+    /** @var array $filter */
+    protected $filter = [];
+
+    public function __construct()
+    {
+        $this->items = new ArrayCollection();
+        parent::__construct();
+    }
+
+    public abstract function getListItemClass(): string;
+
+    public function addItem(DtoInterface $dto)
+    {
+        $this->items->add($dto);
+    }
+
+    public function toArray(): array
+    {
+        $data = parent::toArray();
+        $data['items'] = [];
+        foreach($this->items as $item) {
+            if($item instanceof DtoInterface) {
+                $data['items'] = $item->toArray();
+            }
+        }
+        return $data;
+    }
+
+    public function populate(array $data): void
+    {
+        $listItemClass = $this->getListItemClass();
+        foreach($data as $item) {
+            if(is_array($item)) {
+                $dto = new $listItemClass();
+                if($dto instanceof DtoInterface) {
+                    $dto->populate($item);
+                    $this->addItem($dto);
+                }
+            }
+        }
+
+        $this->setCount($this->items->count());
+        if($this->getTotal() <= 0) {
+            $this->setTotal($this->getCount());
+        }
+
+        $this->setTotalPages(ceil($this->getTotal() / $this->getItemsPerPage()));
+
+        if($this->getTotalPages() > $this->getCurrentPage()) {
+            $this->setNextPage($this->getCurrentPage() + 1);
+        }
+        if($this->getCurrentPage() > 1) {
+            $this->setPreviousPage($this->getCurrentPage() - 1);
+        }
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotal(): int
+    {
+        return $this->total;
+    }
+
+    /**
+     * @param int $total
+     */
+    public function setTotal(int $total): void
+    {
+        $this->total = $total;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCount(): int
+    {
+        return $this->count;
+    }
+
+    /**
+     * @param int $count
+     */
+    public function setCount(int $count): void
+    {
+        $this->count = $count;
+    }
 
     /**
      * @return array
      */
-    public function getItems(): array
+    public function getFilter(): array
     {
-        return $this->items;
+        return $this->filter;
     }
 
     /**
-     * @param array $items
+     * @param array $filter
      */
-    public function setItems(array $items): void
+    public function setFilter(array $filter): void
     {
-        $this->items = $items;
+        $this->filter = $filter;
     }
 }
---
+++ src/Dto/Traits/PaginationTrait.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: siim
+ * Date: 25.01.19
+ * Time: 17:20
+ */
+
+namespace Sf4\Api\Dto\Traits;
+
+trait PaginationTrait
+{
+
+    /** @var int $itemsPerPage */
+    protected $itemsPerPage = 15;
+
+    /** @var int $totalPages */
+    protected $totalPages = 0;
+
+    /** @var int $currentPage */
+    protected $currentPage = 1;
+
+    /** @var int|null $nextPage */
+    protected $nextPage = null;
+
+    /** @var int|null $previousPage */
+    protected $previousPage = null;
+
+    /**
+     * @return int
+     */
+    public function getItemsPerPage(): int
+    {
+        return $this->itemsPerPage;
+    }
+
+    /**
+     * @param int $itemsPerPage
+     */
+    public function setItemsPerPage(int $itemsPerPage): void
+    {
+        $this->itemsPerPage = $itemsPerPage;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotalPages(): int
+    {
+        return $this->totalPages;
+    }
+
+    /**
+     * @param int $totalPages
+     */
+    public function setTotalPages(int $totalPages): void
+    {
+        $this->totalPages = $totalPages;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCurrentPage(): int
+    {
+        return $this->currentPage;
+    }
+
+    /**
+     * @param int $currentPage
+     */
+    public function setCurrentPage(int $currentPage): void
+    {
+        $this->currentPage = $currentPage;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getNextPage(): ?int
+    {
+        return $this->nextPage;
+    }
+
+    /**
+     * @param int|null $nextPage
+     */
+    public function setNextPage(?int $nextPage): void
+    {
+        $this->nextPage = $nextPage;
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getPreviousPage(): ?int
+    {
+        return $this->previousPage;
+    }
+
+    /**
+     * @param int|null $previousPage
+     */
+    public function setPreviousPage(?int $previousPage): void
+    {
+        $this->previousPage = $previousPage;
+    }
+}
