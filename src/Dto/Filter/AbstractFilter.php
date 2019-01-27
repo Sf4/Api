fb70bddc83611ee9dc3f7ae72cd55882dcdb4ef9
Siim Liimand <siim.liimand@gmail.com>
2019-01-27 16:59:59 +0200

    Add filter

---
+++ src/Dto/Filter/AbstractFilter.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: siim
+ * Date: 27.01.19
+ * Time: 16:59
+ */
+
+namespace Sf4\Api\Dto\Filter;
+
+use Sf4\Api\Dto\AbstractDto;
+
+abstract class AbstractFilter extends AbstractDto
+{
+
+}
---
+++ src/Dto/Filter/FilterInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: siim
+ * Date: 27.01.19
+ * Time: 16:58
+ */
+
+namespace Sf4\Api\Dto\Filter;
+
+use Sf4\Api\Dto\DtoInterface;
+
+interface FilterInterface extends DtoInterface
+{
+
+}
