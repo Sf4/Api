4525d773ec10023667420b1a4be8e1318cdbc2cd
Siim Liimand <siim.liimand@gmail.com>
2019-01-28 19:09:44 +0200

    Populate filter

--- src/Dto/Filter/AbstractFilter.php
+++ src/Dto/Filter/AbstractFilter.php
@@ -12,5 +12,17 @@ use Sf4\Api\Dto\AbstractDto;
 
 abstract class AbstractFilter extends AbstractDto implements FilterInterface
 {
-
+    public function populate(array $data): void
+    {
+        $newData = [];
+        foreach($data as $key => $value) {
+            if(isset($value->type) && isset($value->value)) {
+                $filterItem = new BaseFilterItem();
+                $filterItem->setType($value->type);
+                $filterItem->setValue($value->value);
+                $newData[$key] = $filterItem;
+            }
+        }
+        parent::populate($newData);
+    }
 }
