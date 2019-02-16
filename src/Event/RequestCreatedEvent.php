5d707913770ff1605e4b18d8036695ad126b1a3c
Siim Liimand <siim.liimand@gmail.com>
2019-02-16 21:26:48 +0200

    Use RequestTrait and ResponseTrait

--- src/Event/RequestCreatedEvent.php
+++ src/Event/RequestCreatedEvent.php
@@ -9,18 +9,16 @@
 namespace Sf4\Api\Event;
 
 use Sf4\Api\Request\RequestInterface;
-use Sf4\Api\Response\ResponseInterface;
+use Sf4\Api\Request\RequestTrait;
+use Sf4\Api\Response\ResponseTrait;
 use Symfony\Component\EventDispatcher\Event;
 
 class RequestCreatedEvent extends Event
 {
+    use RequestTrait;
+    use ResponseTrait;
+
     const NAME = 'api.request.created';
-
-    /** @var RequestInterface */
-    protected $request;
-
-    /** @var ResponseInterface|null $response */
-    protected $response;
 
     /**
      * RequestCreatedEvent constructor.
@@ -29,37 +27,5 @@ class RequestCreatedEvent extends Event
     public function __construct(RequestInterface $request)
     {
         $this->setRequest($request);
-    }
-
-    /**
-     * @return RequestInterface
-     */
-    public function getRequest(): RequestInterface
-    {
-        return $this->request;
-    }
-
-    /**
-     * @param RequestInterface $request
-     */
-    public function setRequest(RequestInterface $request): void
-    {
-        $this->request = $request;
-    }
-
-    /**
-     * @return ResponseInterface|null
-     */
-    public function getResponse(): ?ResponseInterface
-    {
-        return $this->response;
-    }
-
-    /**
-     * @param ResponseInterface|null $response
-     */
-    public function setResponse(?ResponseInterface $response): void
-    {
-        $this->response = $response;
     }
 }
